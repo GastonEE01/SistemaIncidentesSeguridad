48028f4bd2e2dc7f72e6039740d23af9d4ca9887
GastonEE01 <142426074+GastonEE01@users.noreply.github.com>
2025-06-04 12:35:28 -0300

    Agregar archivos de proyecto.

---
+++ SistemaIncidentesSeguridad/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaIncidentesSeguridad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
