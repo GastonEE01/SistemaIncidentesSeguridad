e7188201bdbc95a9375d5a3e5159f763338772dd
GastonEE01 <142426074+GastonEE01@users.noreply.github.com>
2025-06-15 01:39:16 -0300

    Modifico la BDD
    Agrego las demas tablas,antes solo estaba la tabla Usuario

---
+++ SistemaIncidentesSeguridadEntidad/EF/Comentario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaIncidentesSeguridad.EF;
+
+public partial class Comentario
+{
+    public int Id { get; set; }
+
+    public int IdTicket { get; set; }
+
+    public int IdUsuario { get; set; }
+
+    public string Contenido { get; set; } = null!;
+
+    public DateTime Fecha { get; set; }
+
+    public virtual Ticket IdTicketNavigation { get; set; } = null!;
+
+    public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+}
